b0146b632d47ba0aee1174260fdca7f65414af3e
MattDahEpic <mattdahepic@gmail.com>
2017-12-01 22:41:00 -0700

    menu

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+    public Slider progress;
+    private AsyncOperation load;
+
+    public void Start () {
+        load = SceneManager.LoadSceneAsync("Game");
+        load.allowSceneActivation = false;
+    }
+    public void Update () {
+        progress.value = load.progress;
+    }
+
+    public void OnPlay () {
+        if (load.progress == 0.9f) {
+            load.allowSceneActivation = true;
+        }
+    }
+
+    public void OnExit () {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
