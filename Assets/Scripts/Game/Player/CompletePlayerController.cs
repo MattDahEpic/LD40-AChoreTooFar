439f712498ffd899ad294f67786f815f032f4b53
MattDahEpic <mattdahepic@gmail.com>
2017-12-04 18:19:25 -0700

    get swole after tasks complete

--- Assets/Scripts/Game/Player/CompletePlayerController.cs
+++ Assets/Scripts/Game/Player/CompletePlayerController.cs
@@ -11,6 +11,9 @@ public class CompletePlayerController : MonoBehaviour {
     public SwfClipController render;
 
 	private Rigidbody2D rb2d;
+    private bool swole { get { return TapGameController.plays > 6 && MemoryGameController.plays >= 5 && QTEGameController.plays >= 4 && SimonGameController.plays > 3; } }
+    private string idle_str { get { return swole ? "jake muscle idle" : "jack_idle"; } }
+    private string walk_str { get { return swole ? "jake muscle run" : "jack_walk"; } }
 
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
@@ -27,14 +30,14 @@ public class CompletePlayerController : MonoBehaviour {
         else rb2d.velocity = Vector2.zero;
         //and set the render
 	    if (rb2d.velocity != Vector2.zero) {
-	        render.PlayIfNotAlreadyPlaying("jack_walk");
+	        render.PlayIfNotAlreadyPlaying(walk_str);
 	        if (rb2d.velocity.x > 0) { //moving right
 	            render.transform.localScale = new Vector3(-3,3,3);
             } else { // moving left
 	            render.transform.localScale = new Vector3(3,3,3);
             }
 	    } else {
-	        render.PlayIfNotAlreadyPlaying("jack_idle");
+	        render.PlayIfNotAlreadyPlaying(idle_str);
 	    }
 	}
 
